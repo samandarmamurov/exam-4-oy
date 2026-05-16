6c20f76d219c4e11f7573dcbcbe35fbac46c1684
Samandar <samandarmamurov0409@gmail.com>
2026-05-16 15:23:12 +0500

    add 5-masala

--- app.js
+++ app.js
@@ -132,7 +132,24 @@
 // ● findMax([]) → null
 
 function findMax(arr) {
-    if ()
+    if (arr.length === 0) return null;
+
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
 }
+
+console.log(findMax([1, 5, 3]));
+console.log(findMax([-10, -3, -7]));
+console.log(findMax([]));
+
+
+
 
 
